cdec8424cfef1ec8e93489e4a24d7a4c69b74438
Andrew Dupont <github@andrewdupont.net>
2025-07-23 19:26:21 -0700

    Remove stray files

--- spec/pulsar-find-references-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import PulsarFindReferencesView from '../lib/pulsar-find-references-view';
-
-describe('PulsarFindReferencesView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
